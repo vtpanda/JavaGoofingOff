9bcd07135141533167da907405ba2eacea384604
Matt Roberts <saotomesan@gmail.com>
2022-09-17 11:12:41 -0500

    Additional comments

--- Player/src/Question3.java
+++ Player/src/Question3.java
@@ -22,6 +22,8 @@ public class Question3 {
         //Take the memory address in controller1 (which is the same memory address as controller2),
         // find the Player object at that location in memory
         // and perform the changeOutfit() method on it
+        //Notably, a String object within the Player object will be created and assigned to its internal variable, 
+        // but it doesn't mean that a new Player object will be created
         controller1.changeOutfit();
 
         //Because controller1 and controller2 point to the same object, both will "see" the changes made
